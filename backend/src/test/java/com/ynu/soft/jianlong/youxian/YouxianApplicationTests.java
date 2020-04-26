f7ed68679a39f3809702fecbc3405723b8723f78
JianlongHu <1434938035@qq.com>
2020-04-26 17:43:20 +0800

    初始化

--- backend/src/test/java/com/ynu/soft/jianlong/youxian/YouxianApplicationTests.java
+++ backend/src/test/java/com/ynu/soft/jianlong/youxian/YouxianApplicationTests.java
@@ -1,20 +1,10 @@
 package com.ynu.soft.jianlong.youxian;
 
-import com.ynu.soft.jianlong.youxian.repository.CommodityRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class YouxianApplicationTests {
-
-    @Autowired
-    CommodityRepository commodityRepository;
-
-    @Test
-    void testFindByKeyword(){
-        System.out.println(commodityRepository.findByCnameContains("豆腐"));
-    }
 
     @Test
     void contextLoads() {
